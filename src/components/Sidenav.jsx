8d8756cca924ca8d1153e517f8acb53fdcf4fd6f
Sandra Sanders <sandy-sanders@msn.com>
2024-09-07 09:33:50 -0700

    added postcs.confi file

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,84 +1,80 @@
 import { useState } from "react";
-import { AiOutlineMenu, AiOutlineHome} from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
 
-    
 export default function SideNav() {
   const [nav, setNav] = useState(false);
-  const handleNav = () => {
+
+    function handleNav() {
     setNav(!nav);
-  };
+  }
+  
   return (
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className='absolute top-4 right-4 z-[99] md:hidden'/>
-   
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Home</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Work</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Skills</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Projects</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Certifications</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Education</span>
           </a>
-          <a 
+          <a
             href="main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200">
-  
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200"
+          >
             <AiOutlineHome size={20} />
-          
+
             <span className="pl-4">Resume</span>
           </a>
-
-
         </div>
-
-
-      ) : 
-      
-      (
+      ) : (
         <div></div>
       )}
     </div>
